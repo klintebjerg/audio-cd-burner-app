300c0f4212d54a12007e206d4d346c4a8f5cc5fd
Mathias <mkpstudent@gmail.com>
2025-09-02 20:50:49 +0200

    initialize AudioCdBurner application with basic structure and UI

---
+++ AudioCdBurner.App/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace AudioCdBurner.App
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void AddFilesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "Audio|*.mp3;*.flac;*.wav;*.m4a;*.aac;*.ogg|Alle filer|*.*"
+            };
+
+            if (dlg.ShowDialog() == true)
+            {
+                foreach (var path in dlg.FileNames)
+                    FilesList.Items.Add(path);
+
+                CountText.Text = $"Valgt: {FilesList.Items.Count}";
+            }
+        }
+    }
+}
